cf4410a2a7405507b721f7f561d983d4caaf5604
pedroregispoar <pedroregispoar@gmail.com>
2020-07-08 01:25:36 -0300

    Adiciona install-nix.sh

---
+++ install-nix.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+curl -L https://nixos.org/nix/install | sh
+
+
+nix-channel --list
+
+nix-channel --add https://nixos.org/channels/nixos-20.03 nixpkgs
+
+nix-channel --update
+
+nix-env --install --from-expression \
+"_: with import <nixpkgs/nixos> { configuration = {}; }; with config.system.build; [ nixos-generate-config nixos-install nixos-enter manual.manpages ]"
+# short form
+# nix-env -iE
+
+# nix-env --install --attr nixpkgs.glibcLocales
+
+# https://unix.stackexchange.com/a/187727
+export LC_ALL=C
+
+nix-env --install --attr nixpkgs.glibcLocales
+
+
+export LOCALE_ARCHIVE="$(nix-env --installed --no-name --out-path --query glibc-locales)/lib/locale/locale-archive"
+
+
+sudo `which nixos-generate-config` --root /
+
+
+sudo chown -R 0.0 /nix
+
+
+sudo touch /etc/NIXOS
+
+sudo touch /etc/NIXOS_LUSTRATE
+
+echo etc/nixos | sudo tee -a /etc/NIXOS_LUSTRATE
